66cecc53d064a8a76a8a7d094e5bf88a23ff91f9
NguyenTranDat <21020011@vnu.edu.vn>
2024-01-01 19:10:20 +0700

    abcbabs

--- Backend/src/models/sequelize.js
+++ Backend/src/models/sequelize.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize('fall2324w3g5', 'root', '12345678', {
 });
 
 sequelize.authenticate().then(() => {
-  console.log('Kết nối cơ sở dữ liệu thành công. ', );
+  console.log('Kết nối cơ sở dữ liệu thành công. ' );
 }).catch((error) => {
   console.error('Lỗi kết nối cơ sở dữ liệu:', error);
 });
